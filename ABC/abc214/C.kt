e61e7541bfa5d79c44a8ababe85a0a92f261cc8e
metsu971 <tet.drago.autochess@gmail.com>
2025-04-23 20:27:49 +0900

    correct answer

--- ABC/abc214/C.kt
+++ ABC/abc214/C.kt
@@ -1,33 +1,22 @@
 fun main() {
-
     val N = readLine()!!.toInt()
+    val S = readLine()!!.split(" ").map { it.toLong() }
+    val T = readLine()!!.split(" ").map { it.toLong() }
 
-    val S = readLine()!!.split(" ").map { it.toInt() }
-    val T = readLine()!!.split(" ").map { it.toInt() }
+    val receive = T.toMutableList()
 
-    val firstReceivedTime = mutableListOf<Int>()
-    //Tから先に受け取るのか、Sから先に受け取るのかを考えていく
-    //3番目のスヌけが最初に受け取る時刻はT[3]またはT[2]+S[2+1]の早い方！！
-
-    for (i in 1 until 10 * N) {//Nでループする
-        if (firstReceivedTime.size == N) break
-
-        if (i == 1) {//Siは機能しないので特別扱い
-            firstReceivedTime.add(T[i])
-            continue
-        }
-        //SiとTiの代償比較
-        if (T[i - 1] > T[i - 2] + S[i - 2]) {// i+1番目のすぬけが受け取る時間
-            firstReceivedTime.add(T[i - 1])
-        } else {
-            firstReceivedTime.add(T[i - 2] + S[i - 2])
+    var updated = true
+    while (updated) {
+        updated = false
+        for (i in 0 until N) {
+            val next = (i + 1) % N
+            val newTime = receive[i] + S[i]
+            if (receive[next] > newTime) {
+                receive[next] = newTime
+                updated = true // 1回でも更新されたら、もう1周必要
+            }
         }
     }
 
-    for (i in firstReceivedTime) {
-        println(i)
-    }
-
+    receive.forEach { println(it) }
 }
-
-
