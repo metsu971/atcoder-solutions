2ca1be97b81ed7cc1a82b597eab176d7f751d59a
metsu971 <tet.drago.autochess@gmail.com>
2025-05-18 05:38:48 +0900

    correct answer 1

--- ABC/abc406/B.kt
+++ ABC/abc406/B.kt
@@ -1,12 +1,14 @@
+import kotlin.math.pow
 fun main() {
     val (N, K) = readLine()!!.split(" ").map {it.toInt() }
     val A = readLine()!!.split(" ").map {it.toLong() }
 
+    val limit = 10.0.pow(K).toLong()
     var result = 1L
 
     for (i in 0 until N) {
         result *= A[i]
-        if (result.toString().length > K) {//Longの範囲ギリギリで値が正しく扱えない場合がある
+        if (result >= limit) {//result >= 10^Kで比較
             result = 1L
         }
     }
